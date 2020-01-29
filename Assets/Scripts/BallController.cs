6c27ede64b10883f94b0676171b0d60e86197ae1
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-01-29 21:36:24 +0000

    sdgasg

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
