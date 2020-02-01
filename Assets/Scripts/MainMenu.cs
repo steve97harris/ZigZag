ab64df07fbc4656f3b88976366023b9cff5b5bf8
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-02-01 16:59:28 +0000

    a;lsdkfj

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Level01");
+        
+    }
+
+    public void Stats()
+    {
+        SceneManager.LoadScene("Stats");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
+
