c4b3d1daa9c981830cdf45b9d1b85a5f3d7fa6e6
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-02-03 15:28:34 +0000

    asdfadfs

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
-    
-    
+    public GameObject zigzagPanel;
+    public GameObject gameOverPanel;
+    public Text score;
+    public Text highScore1;
+    public Text highScore2;
     
     void Start()
     {
