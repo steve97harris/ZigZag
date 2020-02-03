155adb8ead9ade2248f8192864a2c254d21f4461
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-02-03 17:32:46 +0000

    fghj

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -2,18 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
+    public static UiManager instance;
+    
     public GameObject zigzagPanel;
     public GameObject gameOverPanel;
+    public GameObject tapText;
     public Text score;
     public Text highScore1;
     public Text highScore2;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
     
     void Start()
     {
         
+    }
+
+    public void GameStart()
+    {
+        tapText.SetActive(false);
+        zigzagPanel.GetComponent<Animator>().Play("panelUP");
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
     }
 
     void Update()
